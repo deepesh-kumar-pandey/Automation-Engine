b0e421de8471a2c42ca470ebaa1b4b21852106e0
Deepesh <deepesh040505@gmail.com>
2026-03-05 22:55:16 +0530

    build: add backend build artifacts excluding vcpkg heavy dependencies

---
+++ Backend/build/CMakeFiles/automation_backend.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/automation_backend.dir/src/main.cpp.o"
+  "CMakeFiles/automation_backend.dir/src/main.cpp.o.d"
+  "CMakeFiles/automation_backend.dir/src/worker/ShellTask.cpp.o"
+  "CMakeFiles/automation_backend.dir/src/worker/ShellTask.cpp.o.d"
+  "automation_backend"
+  "automation_backend.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/automation_backend.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
